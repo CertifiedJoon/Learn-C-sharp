089e016ee3aecb6120cc7ed17d3f252dd75145ce
Joon_Thinkpad <mjypark1212@gmail.com>
2025-03-02 14:14:08 +0800

    Data type range

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,20 +1,19 @@
-﻿int employeeLevel = 400;
-string employeeName = "John Smith";
+﻿Console.WriteLine("Signed integral types: ");
 
-string title = "";
+Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int   : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long  : {long.MinValue} to {long.MaxValue}");
 
-switch (employeeLevel)
-{
-  case 100:
-    title = "Junior Associate";
-    break;
-  case 200:
-    title = "Senior Associate";
-    break;
-  case 300:
-  case 400:
-    title = "Boss";
-    break;
-}
+Console.WriteLine("Unsigned integral types: ");
 
-Console.WriteLine($"{employeeName}, {title}");
+Console.WriteLine($"byte  : {byte.MinValue} to {byte.MaxValue}, 2^{Math.Ceiling(Math.Log2(byte.MaxValue))} numbers");
+Console.WriteLine($"ushort: {ushort.MinValue} to {ushort.MaxValue}, 2^{Math.Ceiling(Math.Log2(ushort.MaxValue))} numbers");
+Console.WriteLine($"uint  : {uint.MinValue} to {uint.MaxValue}, 2^{Math.Ceiling(Math.Log2(uint.MaxValue))} numbers");
+Console.WriteLine($"ulong : {ulong.MinValue} to {ulong.MaxValue}, 2^{Math.Ceiling(Math.Log2(ulong.MaxValue))} numbers");
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types");
+Console.WriteLine($"float: {float.MinValue} to {float.MaxValue} with (~6-9 digits of precision)");
+Console.WriteLine($"double: {double.MinValue} to {double.MaxValue} with (~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} with (~28-29 digits of precision)");
