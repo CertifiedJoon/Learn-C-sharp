7acfc21a8002c238c53e6e803e467822ab053b4c
Joon_Thinkpad <mjypark1212@gmail.com>
2025-03-02 16:09:54 +0800

    methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,59 +1,25 @@
-﻿string[] pallets = [ "B14", "A11", "B12", "A13" ];
+﻿int[] schedule = {800, 1200, 1600, 2000};
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
-
-foreach (string pallet in pallets)
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 {
-    Console.WriteLine($"-- {pallet}");
+    int diff = 0;
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    {
+        Console.WriteLine("Invalid GMT");
+    }
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    } else 
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+    }
+
+    for (int i = 0; i < times.Length; i++) 
+    {
+        int newTime = (times[i] + diff) % 2400;
+        Console.WriteLine($"{times[i]} -> {newTime}");
+    }
 }
 
-Console.WriteLine("");
-Console.WriteLine("Reversed....");
-Array.Reverse(pallets);
-
-foreach (string pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-pallets[4] = "C01";
-pallets[5] = "C02";
-
-foreach (string pallet in pallets)
-{
-    Console.WriteLine($" -- {pallet}");
-}
-
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-string result = String.Join(",", valueArray);
-Console.WriteLine(result);
-
-string[] items = result.Split(',');
-foreach (string item in items)
-{
-    Console.WriteLine(item);
-}
-
-string pangram = "The quick brown fox jumps over the lazy dog";
-string[] words = pangram.Split(" ");
-
-for (int i = 0; i < words.Length; i++)
-{
-    words[i] = String.Join("", words[i].ToCharArray().Reverse());
-}
-
-Console.WriteLine(String.Join(" ", words));
+DisplayAdjustedTimes(schedule, 6, -6);
