64062640c00ab8c4ec2e485c599be9288b415d37
Joon_Thinkpad <mjypark1212@gmail.com>
2025-03-02 14:53:28 +0800

    conversions

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,18 +1,16 @@
-﻿using System.Security;
+﻿int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// Your code here to set result1
+int result1 = Convert.ToInt32(value1 / value2); 
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-decimal sum = 0.0M;
-string msg = "";
-decimal parsed;
-foreach (string value in values) 
-{
-    if (decimal.TryParse(value, out parsed)) 
-    {
-        sum += parsed;
-    } else {
-        msg += value;
-    }
-}
-Console.WriteLine($"Message: {msg}");
-Console.WriteLine($"Total: {sum}");
+// Your code here to set result2
+decimal result2 = value2 / (decimal) value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
