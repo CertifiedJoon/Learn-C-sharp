7dfc9064f4f5662fd56387cea87ee20e22f7ee43
Joon_Thinkpad <mjypark1212@gmail.com>
2025-03-02 15:26:26 +0800

    Arrays and string

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,16 +1,59 @@
-﻿int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+﻿string[] pallets = [ "B14", "A11", "B12", "A13" ];
 
-// Your code here to set result1
-int result1 = Convert.ToInt32(value1 / value2); 
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
 
-// Your code here to set result2
-decimal result2 = value2 / (decimal) value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+foreach (string pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
 
-// Your code here to set result3
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+Console.WriteLine("");
+Console.WriteLine("Reversed....");
+Array.Reverse(pallets);
+
+foreach (string pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (string pallet in pallets)
+{
+    Console.WriteLine($" -- {pallet}");
+}
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] words = pangram.Split(" ");
+
+for (int i = 0; i < words.Length; i++)
+{
+    words[i] = String.Join("", words[i].ToCharArray().Reverse());
+}
+
+Console.WriteLine(String.Join(" ", words));
