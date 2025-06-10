76b7c6d9f81f3ea092a702012c8b73f42b3f2da0
Joon_Thinkpad <mjypark1212@gmail.com>
2025-06-11 01:11:28 +0800

    chapter3

---
+++ Textbook/cs12dotnet8/Chapter03-vscode/excercises/Program.cs
@@ -0,0 +1,19 @@
+﻿string? num1 = ReadLine();
+string? num2 = ReadLine();
+
+try
+{
+  if (num1 == null || num2 == null)
+  {
+    Write("null input");
+    return;
+  }
+  int int1 = int.Parse(num1);
+  int int2 = int.Parse(num2);
+
+  WriteLine($"{int1} divided by {int2} is {int1 / int2}");
+}
+catch (FormatException)
+{
+  WriteLine("Input string was not in a correct format");
+}
