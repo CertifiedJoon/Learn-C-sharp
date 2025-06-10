857a7ea83ac91cd148ab2f0c3c7e42964b287eb8
Joon_Thinkpad <mjypark1212@gmail.com>
2025-06-10 13:51:16 +0800

    Arguments

--- Textbook/cs12dotnet8/Chapter02-vscode/Arguments/Program.cs
+++ Textbook/cs12dotnet8/Chapter02-vscode/Arguments/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Arguments
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // WriteLine($"We have {args.Length} arguments");
+      // foreach (string arg in args)
+      // {
+      //   WriteLine(arg);
+      // }
+
+      if (args.Length < 3)
+      {
+        WriteLine("You must specify two colors and cursor size, e.g.");
+        WriteLine("dotnet run red yellow 50");
+        return;
+      }
+
+      ForegroundColor = (ConsoleColor)Enum.Parse(
+        enumType: typeof(ConsoleColor),
+        value: args[0], ignoreCase: true
+      );
+
+      BackgroundColor = (ConsoleColor)Enum.Parse(
+        enumType: typeof(ConsoleColor),
+        value: args[1], ignoreCase: true
+      );
+
+      CursorSize = int.Parse(args[2]);
+    }
+  }
+}
