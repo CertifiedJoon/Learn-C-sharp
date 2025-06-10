e2555fa7f30c95ac35af81b71ff36eb2a11dc384
Joon_Thinkpad <mjypark1212@gmail.com>
2025-06-10 13:42:20 +0800

    Variuables

---
+++ Textbook/cs12dotnet8/Chapter02-vscode/Variables/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml;
+
+object height = 1.88;
+object name = "Amir";
+
+Console.WriteLine($"{name} is {height} meters tall.");
+
+//int length1 = name.Length;
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} chatacters.");
+
+dynamic something;
+
+something = new[] { 3, 5, 7 };
+// something = 12;
+// something = "Ahmed";
+Console.WriteLine($"The length of something is {something.Length}");
+
+Console.WriteLine($"something is a {something.GetType()}");
+
+var population = 67_000_000;
+var weight = 1.88;
+var price = 4.99M;
+var fruit = "Apples";
+var letter = 'Z';
+var happy = true;
+
+
+// Good use of var. readers can infer the type from constructor.
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+
+
+// bad use of var. readers cannot infer the type easily.
+var file1 = File.CreateText("Something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
+
+// target-typed new.
+XmlDocument xml3 = new();
+
+Console.WriteLine($"defualt(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+int number = 13;
+Console.WriteLine($"Number is : {number:N0}");
+number = default;
+Console.WriteLine($"Number is set to default : {number:N0}");
--- Textbook/cs12dotnet8/Chapter02-vscode/Vocabulary/Program.cs
+++ Textbook/cs12dotnet8/Chapter02-vscode/Vocabulary/Program.cs
@@ -1,5 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-//#error version
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
 
-WriteLine($"Computer named {Env.MachineName} says \"No.\"");
+// Declare some unused variables using types in
+// additional assemblies to make them load too.
+
+System.Data.DataSet ds = new();
+HttpClient client = new();
+
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+if (myApp is null) return;
+
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+  Assembly a = Assembly.Load(name);
+  int methodCount = 0;
+
+  foreach (TypeInfo t in a.DefinedTypes)
+  {
+    methodCount += t.GetMethods().Length;
+  }
+
+  WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount,
+    arg2: name.Name);
+}
