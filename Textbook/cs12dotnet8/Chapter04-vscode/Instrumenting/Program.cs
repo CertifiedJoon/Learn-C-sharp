9a8c7bab7ab0810b415392e9c3ff3a7b3fc3432a
Joon_Thinkpad <mjypark1212@gmail.com>
2025-06-11 13:12:49 +0800

    Instrumenting

---
+++ Textbook/cs12dotnet8/Chapter04-vscode/Instrumenting/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine($"Writing to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+#if DEBUG
+// text writer is abuffered so this option calls
+// Flush() on all listeners after writing
+Trace.AutoFlush = true;
+#endif
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+string settingsFile = "appsettings.json";
+
+string settingsPath = Path.Combine(Directory.GetCurrentDirectory(), settingsFile);
+
+Console.WriteLine("Processing: {0}, settingsPath");
+Console.WriteLine("--{0} contents--", settingsFile);
+Console.WriteLine(File.ReadAllText(settingsPath));
+Console.WriteLine("----");
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory());
+
+builder.AddJsonFile(settingsFile, optional: false, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+  displayName: "PacktSwitch",
+  description: "This switch is set via a JSON config."
+);
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Console.WriteLine($"Trace switch value: {ts.Value}");
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+
+int unitsInStock = 12;
+LogSourceDetails(unitsInStock > 10);
+
+Debug.Close();
+Trace.Close();
+
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
